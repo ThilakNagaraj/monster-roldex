e4996d4a93e77f34ca36c3288741b77fb86c9379
thilaknagaraj <thilak.gowda@gmail.com>
2019-09-20 07:44:12 +0530

    Updates

---
+++ precache-manifest.5ddc6e6397b7ed194432e750d07fad0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979d849f320eafd8eeb50fbe80428770",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8ebbbf6d1b5a302f6f54",
+    "url": "/monsters-rolodex/static/css/main.fd7a2bdb.chunk.css"
+  },
+  {
+    "revision": "4dacb04e209306afa2c8",
+    "url": "/monsters-rolodex/static/js/2.eafe2c70.chunk.js"
+  },
+  {
+    "revision": "8ebbbf6d1b5a302f6f54",
+    "url": "/monsters-rolodex/static/js/main.3a8e1da1.chunk.js"
+  },
+  {
+    "revision": "9b25930c882f07be8ee2",
+    "url": "/monsters-rolodex/static/js/runtime~main.470f53b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
